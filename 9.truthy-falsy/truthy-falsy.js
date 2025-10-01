af21b013520fd3f704f7f8b42f5025d5798406eb
ade <kurniawanadeputra17@gmail.com>
2025-10-01 21:43:22 +0700

    [ADD] truthy and falsy

---
+++ 9.truthy-falsy/truthy-falsy.js
@@ -0,0 +1,25 @@
+// truthy? falsy? true? false?
+// const codition = 0 > 3; // false
+
+// const age = 20;
+// const username = "";
+
+// if (age) {
+//     //true
+//   console.log("masuk if")
+// }
+
+// console.log(Boolean(username));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+
+
+////////////////////////////
+const username = "";
+
+if (username) {
+        //true
+    console.log("Hello " + username); 
+} else {
+    console.log("Please Input a username");
+}
