4899e871517841f9d873475650ce00f7d32594dd
ade <kurniawanadeputra17@gmail.com>
2025-10-01 23:28:35 +0700

    [ADD] Array done

---
+++ 11.array/array.js
@@ -0,0 +1,18 @@
+//  const names = ["Ade", "Budi", "Cici", "Deni"];
+
+// names.splice(1, 1);
+// names.length
+// names.push("Eka");
+// names.reverse();
+// names[2] = "Eka"; // boleh ganti value jadi = 1, undefined, dll
+
+// console.log(names);
+
+
+const grid = [
+    [1, 2, 3], // index 0
+    [4, 5, 6], // index 1
+    [7, 8, 9]  // index 2
+];
+
+console.log(grid[1][2]);
