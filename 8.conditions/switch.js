5f41aea56844a2a7e2e468b2ff1c07c02a96af47
ade <kurniawanadeputra17@gmail.com>
2025-10-01 21:17:54 +0700

    [ADD] condition switch done

---
+++ 8.conditions/switch.js
@@ -0,0 +1,18 @@
+const myAnswer = "B";
+
+switch (myAnswer) {
+    case "A":
+        console.log("Jawaban salah");
+        break
+    case "B":
+        console.log("Jawaban Salah");
+        break
+    case "C":
+        console.log("Jawaban Benar");
+        break
+    case "D":
+        console.log("Jawaban salah");
+        break
+    default:
+        console.log("Input Jawaban Salah")
+}
